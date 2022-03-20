87b760553b83053ed7c1d549f3613379a35c7722
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2022-03-20 15:05:15 +0100

    added js for scroll fade

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,7 @@
 const baseUrl = "https://chipr.api.fdnd.nl/projects"
 const h2 = document.querySelector(".hero_section h2")
 const id = 1; 
+const reveals = document.querySelectorAll(".Beyco")
 
 fetch (baseUrl)
 .then ((res)=> {
@@ -14,8 +15,24 @@ fetch (baseUrl)
 })
 
 function renderData(){
-    
-
 }
+
+//Scrol Animatiom//
+
+function reveal() {
+      for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
 
 
