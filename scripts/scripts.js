123c75cb3624d93aef48b896b1ca0ed16d0ff45b
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2022-03-20 15:12:30 +0100

    fetch api

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,24 @@
+const baseUrl = "https://chipr.api.fdnd.nl/projects"
+const h2 = document.querySelector(".hero_section h2")
+const id = 1; 
 const reveals = document.querySelectorAll(".Beyco")
 
+fetch (baseUrl)
+.then ((res)=> {
+    return res.json();
+})
+.then ((data)=>{
+    beycoData =data.find((project)=> project.id===1)
+    console.log(beycoData);
+    h2.innerText = `${beycoData.title}`
+
+})
+
+function renderData(){
+}
 
 
-
-
+//Scrol animation//
 
 function reveal() {
     for (var i = 0; i < reveals.length; i++) {
